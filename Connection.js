4d25ea7643b1a551f4382ce58f33fa911a5453e1
shayan <mkshayan100@gmail.com>
2025-11-25 12:51:30 +0530

    atlas

--- Connection.js
+++ Connection.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
 
-const MONGO_URI = process.env.MONGO_URI || "mongodb://127.0.0.1:27017/ecommerce";
+const MONGO_URI = process.env.MONGO_URI  
 
 export default async function Connection() {
   try {
